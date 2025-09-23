783d578f344a18072ff366880166bc49febb38e4
ignavine <ignavine55@gmail.com>
2025-09-23 05:56:55 -1200

    Worrked on the responsiveness on two of my pages

--- srcipts/script.js
+++
@@ -1,9 +0,0 @@
-       
-document.getElementById("submit-btn").addEventListener("click", function(){
-    document.getElementById("err").innerText="Message sent successfully";
-});
-        document.getElementById("email").addEventListener("input", function () {
-  const email = this.value;
-  const isValid = /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/.test(email);
-  this.style.borderColor = isValid ? "green" : "red";
-});
